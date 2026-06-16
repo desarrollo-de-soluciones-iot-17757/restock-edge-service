26317fc264d7bfc76ac4ab380e0593c6f76ae2e1
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-06-16 15:42:06 -0500

    feat(register-thresholds): add domain service for the threshold entity.

---
+++ devices/domain/services.py
@@ -0,0 +1,50 @@
+from devices.domain.entities import DeviceThreshold
+
+
+class DeviceThresholdService:
+    """
+    Domain service responsible for the creation of valid device threshold entities.
+    """
+
+    @classmethod
+    def create_threshold_for_device(cls,
+                         device_id: str,
+                         custom_supply_weight: float,
+                         custom_supply_unit_measurement: str,
+                         minimum_humidity_percentage: float,
+                         maximum_humidity_percentage: float,
+                         minimum_temperature_in_celsius: float,
+                         maximum_temperature_in_celsius: float,
+                         ) -> DeviceThreshold:
+        """
+        Method to create a DeviceThreshold entity with validated data.
+
+        :param device_id: The device id of the device
+        :param custom_supply_weight: The supply weight of the device
+        :param custom_supply_unit_measurement: The unit measurement of the device
+        :param minimum_humidity_percentage: The minimum humidity percentage of the device
+        :param maximum_humidity_percentage: The maximum humidity percentage of the device
+        :param minimum_temperature_in_celsius: The minimum temperature in Celsius
+        :param maximum_temperature_in_celsius: The maximum temperature in Celsius
+
+        :return: A DeviceThreshold entity
+        """
+        try:
+            parsed_custom_supply_weight = float(custom_supply_weight)
+            parsed_custom_supply_unit_measurement = str(custom_supply_unit_measurement)
+            parsed_minimum_humidity_percentage = float(minimum_humidity_percentage)
+            parsed_maximum_humidity_percentage = float(maximum_humidity_percentage)
+            parsed_minimum_temperature_in_celsius = float(minimum_temperature_in_celsius)
+            parsed_maximum_temperature_in_celsius = float(maximum_temperature_in_celsius)
+        except (ValueError, TypeError):
+            raise ValueError("Invalid data format")
+
+        return DeviceThreshold(
+            device_id=device_id,
+            custom_supply_weight=parsed_custom_supply_weight,
+            custom_supply_unit_measurement=parsed_custom_supply_unit_measurement,
+            minimum_humidity_percentage=parsed_minimum_humidity_percentage,
+            maximum_humidity_percentage=parsed_maximum_humidity_percentage,
+            minimum_temperature_in_celsius=parsed_minimum_temperature_in_celsius,
+            maximum_temperature_in_celsius=parsed_maximum_temperature_in_celsius,
+        )
